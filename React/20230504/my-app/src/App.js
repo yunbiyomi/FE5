c164c179bd9a365769c9a8375ddd63f473894fb1
yunbiyomi <yuyu5850@daum.net>
2023-05-04 16:03:34 +0900

    20230504

---
+++ React/20230504/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TripList from './components/tripList/TripList';
+
+function App() {
+
+  return (
+    <div>
+      <TripList />
+    </div>    
+  );
+}
+
+export default App;
---
+++ React/20230504/my-app/src/components/tripList/TripList.jsx
@@ -0,0 +1,21 @@
+import './TripList.css'
+
+let list = [
+    { no: 1, area: "대전", visited: false },
+    { no: 2, area: "부산", visited: true },
+    { no: 3, area: "목포", visited: false },
+    { no: 4, area: "제주도", visited: false },
+];
+
+function TripList() {
+
+    let areas = list.map((item) => {
+        return (
+            <li key={item.no} className={item.visited ? "list-area-item active" : "list-area-item"}>{item.area}</li>
+            );
+    });
+
+    return <ul className='list-area'>{areas}</ul>
+}
+
+export default TripList;
